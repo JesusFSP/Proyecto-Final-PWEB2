b2044f1fc4580590fe18a2c8457173cd1c1642ca
JesusFSP <jsilva@unsa.edu.pe>
2025-07-25 01:33:43 -0500

    Se añadio el modelo Mesa

--- backend/reservas/models.py
+++ backend/reservas/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Mesa(models.Model):
+    numero = models.PositiveIntegerField(unique=True)
+    capacidad = models.PositiveIntegerField()
+    ubicacion = models.CharField(max_length=50)
+
+
 class Reserva(models.Model):
     nombre_cliente = models.CharField(max_length=100)
     correo_cliente = models.EmailField()
@@ -13,6 +19,7 @@ class Reserva(models.Model):
                 ('cancelada', 'Cancelada')],
         default='pendiente')
     creado_en = models.DateTimeField(auto_now_add=True)
+    mesa = models.ForeignKey(Mesa, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Reserva de {self.nombre_cliente} para el {self.fecha_reserva}"
