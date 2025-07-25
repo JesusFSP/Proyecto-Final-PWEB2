14619dd7a54223ec19987f689d5625b4040d23c2
JesusFSP <jsilva@unsa.edu.pe>
2025-07-25 00:46:52 -0500

    Se configuro las vistas basicas del proyecto

--- backend/reservas/views.py
+++ backend/reservas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Reserva
 
-# Create your views here.
+def home(request):
+    return render(request, 'reservas/home.html')
+
+def lista_reservas(request):
+    reservas = Reserva.objects.all()
+    return render(request, 'reservas/lista_reservas.html', {'reservas': reservas})
+
+def crear_reserva(request):
+    return render(request, 'reservas/crear_reserva.html')
