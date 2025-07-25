72e8e6d3baedcf7f7d0587dfcb745fad29d3ac00
JesusFSP <jsilva@unsa.edu.pe>
2025-07-25 00:41:10 -0500

    Implementando el modelo reservas y haciendo un migracion inicial

--- backend/reservas/models.py
+++ backend/reservas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Reserva(models.Model):
+    nombre_cliente = models.CharField(max_length=100)
+    correo_cliente = models.EmailField()
+    telefono_cliente = models.CharField(max_length=15)
+    fecha_reserva = models.DateField()
+    hora_reserva = models.TimeField()
+    cantidad_personas = models.PositiveIntegerField()
+    estado = models.CharField(max_length=20,
+        choices=[('pendiente', 'Pendiente'),
+                ('confirmada', 'Confirmada'),
+                ('cancelada', 'Cancelada')],
+        default='pendiente')
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reserva de {self.nombre_cliente} para el {self.fecha_reserva}"
