d34c7508e902a964b90ab52197f976fe457b2bc0
JesusFSP <jsilva@unsa.edu.pe>
2025-07-25 01:13:58 -0500

    Se extendio el controlador para angularJS, se agrego la vista de disponibilidad en Django

--- backend/reservas/api.py
+++ backend/reservas/api.py
@@ -1,7 +1,31 @@
 from rest_framework import generics
 from .models import Reserva
 from .serializers import ReservaSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.utils import timezone
+from datetime import timedelta
 
 class ReservaListCreate(generics.ListCreateAPIView):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializer
+
+
+class DisponibilidadView(APIView):
+    def get(self, request):
+        fecha = request.query_params.get('fecha')
+        hora = request.query_params.get('hora')
+        personas = int(request.query_params.get('personas', 1))
+
+        hora_reserva = timezone.datetime.strptime(f"{fecha} {hora}", "%Y-%m-%d %H:%M")
+        hora_inicio = hora_reserva - timedelta(hours=2)
+        hora_fin = hora_reserva + timedelta(hours=2)
+
+        reservas_existentes = Reserva.objects.filter(
+            fecha_reserva=fecha,
+            hora_reserva__range=(hora_inicio.time(), hora_fin.time())
+        ).count()
+
+        disponible = reservas_existentes < 10
+
+        return Response({'disponible': disponible, 'mesas_disponibles': 10 - reservas_existentes})
--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,1 +1,27 @@
 var app = angular.module('SazonPeruanaApp', []);
+
+app.controller('ReservaController', function($scope, $http) {
+    $scope.reserva = {};
+    $scope.disponibilidad = null;
+
+    $scope.verificarDisponibilidad = function() {
+        if ($scope.reserva.fecha_reserva && $scope.reserva.hora_reserva && $scope.reserva.cantidad_personas) {
+            $http.get('/api/disponibilidad/', {
+                params: {
+                    fecha: $scope.reserva.fecha_reserva,
+                    hora: $scope.reserva.hora_reserva,
+                    personas: $scope.reserva.cantidad_personas
+                }
+            }).then(function(response) {
+                $scope.disponibilidad = response.data.disponible;
+            });
+        }
+    };
+
+    $scope.$watchGroup([
+        'reserva.fecha_reserva',
+        'reserva.hora_reserva',
+        'reserva.cantidad_personas'
+    ], $scope.verificarDisponibilidad);
+
+});
