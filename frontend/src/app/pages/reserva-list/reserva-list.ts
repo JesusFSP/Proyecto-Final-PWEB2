594c3fa617c021d98ec345ef3a4ecaefc4555e06
JesusFSP <jsilva@unsa.edu.pe>
2025-07-27 21:22:49 -0500

    Generando servicios y componentes

---
+++ frontend/src/app/pages/reserva-list/reserva-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-list',
+  imports: [],
+  templateUrl: './reserva-list.html',
+  styleUrl: './reserva-list.scss'
+})
+export class ReservaList {
+
+}
