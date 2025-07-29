cf994fff0f721cab0fd1e6c377a9fab2f338267f
JesusFSP <jsilva@unsa.edu.pe>
2025-07-28 19:20:40 -0500

    Creando el modulo home para la pagina de inicio del frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ReservaListComponent } from './pages/reserva-list';
 import { ReservaFormComponent } from './pages/reserva-form';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: ReservaListComponent },
   { path: 'nueva', component: ReservaFormComponent },
   { path: '**', redirectTo: '' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home').then(m => m.Home) },
 ];
